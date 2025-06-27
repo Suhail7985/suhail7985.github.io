08339c74c394e6a51d52cfaf253847daa6edee28
Suhail7985 <suhailmansoori7985@gmail.com>
2025-06-27 16:53:50 +0530

    27june-1

--- MongoDB/q7.js
+++ MongoDB/q7.js
@@ -38,4 +38,135 @@ db.posts.aggregate([
             as: "comments"
             }
         },
+        { $unwind: "$comments" },
+        { $project: { post: 1, comments: 1 } }
+])
+
+
+db.comments.insertOne({ _id: "c6", pid: "p1", comment: "Comment 6" })
+
+
+//
+
+// marks
+// name,term,subject,Score 
+// john,t1,maths,98
+// john,t2,maths,90
+// john,t3,maths,88
+// john,t1,science,92
+// john,t2,science,82
+// john,t3,science,82
+// Cathy,t1,maths,92
+// Cathy,t2,maths,92
+// Cathy,t3,maths,82
+// Cathy,t1,science,92
+// Cathy,t2,science,82
+// Cathy,t3,science,80
+
+db.marks.insertMany([
+    { name: "John", term: "t1", subject: "maths", Score: "98" },
+    { name: "John", term: "t2", subject: "maths", Score: "90" },
+    { name: "John", term: "t3", subject: "maths", Score: "88" },
+    { name: "John", term: "t1", subject: "science", Score: "92" },
+    { name: "John", term: "t2", subject: "science", Score: "82" },
+    { name: "John", term: "t3", subject: "science", Score: "82" },
+    { name: "Cathy", term: "t1", subject: "maths", Score: "92" },
+    { name: "Cathy", term: "t2", subject: "maths", Score: "92" },
+    { name: "Cathy", term: "t3", subject: "maths", Score: "82" },
+    { name: "Cathy", term: "t1", subject: "science", Score: "92" },
+    { name: "Cathy", term: "t2", subject: "science", Score: "82" },
+    { name: "Cathy", term: "t3", subject: "science", Score: "80" }
+])
+
+
+
+//display Average score of each student name wise
+db.marks.aggregate([
+    {
+        $group: {
+            _id: "$name",
+            AvgScore: { $avg: "$Score" }}},
+            {$sort:{_id:1}}
+
+])
+
+
+
+//display Average score of each student  term wise 
+db.marks.aggregate([    
+    {
+        $group: { _id: { name: "$name", term: "$term" },
+        average: { $avg: "$Score" } }
+},
+{ $project: { _id: 0, name: "$_id.name", term: "$_id.term", average: 1 } }
+])
+
+
+// display Average score of each student subject wise
+db.marks.aggregate([
+    {
+        $group: {
+            _id: {term:"$term",subject:"$subject"},
+            AvgScore: { $avg: "$Score" }}},
+            {$sort:{_id:1}}
+
+])
+
+
+// display Average score of each student subject wise and term wise
+db.marks.aggregate([
+{
+    $group: {
+        _id: {name:"$name",term:"$term",subject:"$subject"},
+        AvgScore: { $avg: "$Score" }}},
+        {$sort:{_id:1}}
+    ])
+
+
+
+// studentInfo
+// _id,name
+// s1,John
+// s2,Cathy
+
+db.studentInfo.insertMany([
+    { _id: "s1", name: "John" },
+    { _id: "s2", name: "Cathy" }
+
+])
+
+//rename John to s1 from collection marks
+db.marks.updateMany(
+  {},
+  { $rename: { name: "sid" } }
+)
+
+
+db.marks.updateMany(
+  {sid:"John"},
+  { $set: { sid: "s1" } }
+)
+
+    db.marks.updateMany(
+    {sid:"Cathy"},
+    { $set: { sid: "s2" } }
+    )
+
+//show the name form studentInfo collection and further details from marks
+db.studentInfo.aggregate([
+    {
+        $lookup: {
+            from: "marks",
+            localField: "_id",
+            foreignField: "sid",
+            as: "marks"
+            }
+            },
+            { $unwind: "$marks" },
+            {$group: {
+                _id: "$marks.term",
+               
+                AvgScore: { $avg: "$marks.Score" }
+            }
+}       
 ])
